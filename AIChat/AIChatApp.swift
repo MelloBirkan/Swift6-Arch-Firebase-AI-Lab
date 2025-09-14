08ee107b1ef88657fcb954741f11b69bcfddd366
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-09-14 18:22:09 -0300

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Marcello Gonzatto Birkan on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
